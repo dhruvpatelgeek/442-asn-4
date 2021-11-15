e6aeb55b35b357024e7e50790c08292e01c77207
dhruvpatelgeek <dhruvpatel@vm-instt-min.us-central1-a.c.dhruv-22.internal>
2021-11-15 22:02:36 +0000

    added variable file name support

--- q3/task3/password_changer.py
+++ q3/task3/password_changer.py
@@ -1,6 +1,6 @@
 import hashlib
-
-with open('t3.exe', 'rb') as f:
+fileName = input('enter file name: ')
+with open(fileName, 'rb') as f:
     hexdata = f.read().hex()
 flagpole ="396d4d70353266734e24" # find the location where the SHA-1 of the password is stored
 loc=hexdata.find(flagpole)+len(flagpole)
@@ -13,8 +13,8 @@ hash_object = hashlib.sha1(new_password.encode())
 new_password_hash=hash_object.hexdigest()
 print("HASH of the NEW password is  <"+new_password_hash+">");
 
-#since strings are immutable in python im just recrating the entire program 
-#it is a very inefficient way to do this 
+#since strings are immutable in python im just recrating the entire program
+#it is a very inefficient way to do this
 slice_a=hexdata[:loc]
 slice_b=new_password_hash
 slice_c=hexdata[loc+40:]
@@ -29,3 +29,4 @@ print("HASH of the CHANGED password <"+new_program[loc:loc+40]+">");
 print("[writing output.exe]")
 
 open('Output.exe', 'wb').write(result)
+
